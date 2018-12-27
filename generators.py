da52034c977fade047904a0b4d23e8f9d0ff3b98
gmdmgithub <gmika@interia.pl>
2018-12-27 01:34:45 +0100

    generetor more complex example

--- generators.py
+++ generators.py
@@ -1,3 +1,10 @@
+ #pip install memory_profiler
+
+import memory_profiler as mem_profile
+import random
+import time
+
+
 def square_numbers(nums):
     for i in nums:
         yield (i*i) #yield keyword create generators
@@ -11,10 +18,55 @@ print(list(my_nums))
 for my_num in my_nums:
     print(my_num)
 
-
-
+#by the list comprehension - but with bracets is a generator!
 my_nums2 = (x * x for x in [2, 3, 4, 5, 6])
+print(my_nums2)
 print(list(my_nums2))
 
-# for num in my_nums:
-#     print(num)
+
+print('################### MORE COMPLEX ###############################')
+
+names = ['John', 'Corey', 'Adam', 'Steve', 'Rick', 'Thomas']
+majors = ['Math', 'Engineering', 'CompSci', 'Arts', 'Business']
+
+# print('Memory (Before): {}Mb '.format(mem_profile.memory_usage_psutil()))
+print('Memory (Before): ' + str(mem_profile.memory_usage()) + 'MB')
+
+
+def people_list(num_people):
+    result = []
+    for i in range(num_people):
+        person = {
+            'id': i,
+            'name': random.choice(names),
+            'major': random.choice(majors)
+        }
+        result.append(person)
+    return result
+
+
+def people_generator(num_people):
+    for i in range(num_people):
+        person = {
+            'id': i,
+            'name': random.choice(names),
+            'major': random.choice(majors)
+        }
+        yield person
+
+
+#huge difference between Generator (yield) and list!!
+
+# t1 = time.clock()
+# people = people_list(1000000) # never convert into list - you lose everything
+# t2 = time.clock()
+
+t1 = time.clock()
+people = people_generator(1000000)
+t2 = time.clock()
+
+# print 'Memory (After) : {}Mb'.format(mem_profile.memory_usage_psutil())
+print('Memory (After) : ' + str(mem_profile.memory_usage()) + 'MB')
+
+# print 'Took {} Seconds'.format(t2-t1)
+print('Took ' + str(t2 - t1) + ' Seconds')
