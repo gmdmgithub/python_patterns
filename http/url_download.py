4b5b4c2dade870b4ecf5ad5ff639877d78973db3
gmdmgithub <gmika@interia.pl>
2019-01-03 21:58:22 +0100

    params for urllib

--- http/url_download.py
+++ http/url_download.py
@@ -1,8 +1,16 @@
 from urllib import request
+from urllib import parse
+import urllib
 
 try:
+
+    #build parameters
+    parmas = {"v":"EuC-yVzHhMI","t":"5m56s"}
+    q_string = parse.urlencode(parmas)
+    print(q_string)
+
     req = request.urlopen('https://stackoverflow.com')
-    cont = req.read()
+    cont = req.read() 
     #just in one line
     request.urlretrieve('https://www.python.org/static/img/python-logo@2x.png','python.png')
     # print(cont)
@@ -12,5 +20,9 @@ except Exception as e:
 
 
 f = open('python_stack.html','wb')
+# print(dir(urllib))
+# print(dir(request))
+# type(cont) # this is a binary 
 f.write(cont)
+#print(cont.decode('UTF-8')) #to print it as a string
 f.close()
