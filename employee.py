b7559777baaea1372e148a4d85deefbff71179c3
gmdmgithub <gmika@interia.pl>
2019-01-27 23:39:04 +0100

    @property and @attribute.setter decorator for the class

--- employee.py
+++ employee.py
@@ -9,10 +9,21 @@ class Employee:
     def __init__(self, first, last, pay=6000):
         self.first = first
         self.last = last
-        self.email = first + '.' + last + '@email.com'
+        # self.email = first + '.' + last + '@email.com'
         self.pay = pay
 
         Employee.num_of_emps += 1
+
+    @property  # now accesable like attribute
+    def email(self):
+        try:  #check if the attibute was set
+            return self.__email
+        except AttributeError:
+            return f"{self.first}.{self.last}@email.com"
+
+    @email.setter
+    def email(self, new_email):
+        self.__email = new_email
 
     def fullname(self):
         return '{} {}'.format(self.first, self.last)
@@ -20,8 +31,11 @@ class Employee:
     def apply_raise(self):
         self.pay = int(self.pay * self.raise_amt)
 
-    def __repr__(self):
-        return f'Hi there {self.first}, {self.last} and my salary is: {self.pay}'
+    def __repr__(self):  #usually reprent represent like constructor
+        return f"Employee('{self.first}','{self.last}',{self.pay})"
+
+    def __str__(self):  #str representrs to_string
+        return f'Hi there {self.first}, {self.last} and my salary is: {self.pay} my email is {self.email}'
 
     @classmethod
     def set_raise_amt(cls, amount):
@@ -70,6 +84,8 @@ emp_str_1 = 'John-Doe-8000'
 new_emp_1 = Employee.from_string(emp_str_1)
 
 print(new_emp_1)
+print(new_emp_1.__repr__())
+emp_2.email = "adam.colt@gmail.com"
 
 my_date = datetime.date(2016, 7, 11)
 
