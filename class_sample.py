000e8bafdafe18b86eda6dd8abaf95070e859b27
gmdmgithub <gmika@interia.pl>
2019-01-27 17:25:19 +0100

    inheritance

--- class_sample.py
+++ class_sample.py
@@ -2,10 +2,11 @@ print('################### simple example ############################')
 
 
 class Person:
-    def __init__(self,name):
+    def __init__(self, name):
         self.__hands = 2
         self.__legs = 2
         self.__name = name
+
     def nimbles(self):
         print(f'person has {self.__hands} hands and {self.__legs} legs')
 
@@ -16,14 +17,15 @@ class Person:
 alan = Person('Alan')
 #alan.__hands - cause compilation error
 alan.nimbles()
-print('Strange access to private field - called "Mangling"',alan._Person__name)
-print(alan) # to string methot should be called (__repr__)
+print('Strange access to private field - called "Mangling"',
+      alan._Person__name)
+print(alan)  # to string methot should be called (__repr__)
 
 print('################### encapsulation ############################')
 
 
 class Patient:
-    def set_id(self,id):
+    def set_id(self, id):
         self.__id = id
 
     def set_name(self, name):
@@ -41,45 +43,61 @@ class Patient:
     def get_ssn(self):
         return self.__ssn
 
+
 patient = Patient()
 
 patient.set_id(1)
 patient.set_name('Alex')
 patient.set_ssn('AAC-1234')
-print(patient.get_id(), patient.get_name(),patient.get_ssn())
+print(patient.get_id(), patient.get_name(), patient.get_ssn())
 
 print('################### inheritance ############################')
-class Iphon:
-    def __init__(self,name, size):
+
+
+class IPhone:
+    def __init__(self, name, size):
         self.__name = name
         self.__size = size
+
     def __repr__(self):
         return f'Name from toString is {self.__name}'
+
     def show(self):
         print(f'the best phone in the world is {self.__name}')
 
-class IphonX(Iphon):
-    def __init__(self,name, size,headphones):
-        super().__init__(name,size)
+    def get_name(self):
+        return self.__name
+
+
+class IPhoneX(IPhone):
+    def __init__(self, name, size, headphones):
+        super().__init__(name, size)
         self.__headphones = headphones
 
+    def ring(self):
+        print(f'Hi here iphonex is ringing {self.get_name()}')
 
-iphx = IphonX('X',10,False)
+
+iphx = IPhoneX('model X', 10, False)
 iphx.show()
+iphx.ring()
 print(iphx)
 
-print('################### abstrac class - interface ############################')
+print(
+    '################### abstrac class - interface ############################'
+)
 #in python its a class with all abstract methods @abstractmethod
 from abc import abstractmethod, ABC
 
-class Car(ABC): #must inherit from ABC class
 
+class Car(ABC):  #must inherit from ABC class
     @abstractmethod
     def drive(self):
         pass
 
+
 class BMW(Car):
-    def __init__(self,name):
+    def __init__(self, name):
         self.__name = name
 
     def drive(self):
@@ -88,13 +106,13 @@ class BMW(Car):
     def __repr__(self):
         return f'I am a {self.__name}'
 
+
 bmw = BMW('BMW')
 bmw.drive()
 print(bmw)
 
 
 class TouchScreanLaptop(ABC):
-
     @abstractmethod
     def scroll(self):
         pass
@@ -102,6 +120,7 @@ class TouchScreanLaptop(ABC):
     @abstractmethod
     def click(self):
         pass
+
 
 class HP(TouchScreanLaptop):
     def scroll(self):
@@ -112,6 +131,7 @@ class DELL(TouchScreanLaptop):
     def scroll(self):
         print('DELL is scrolling')
 
+
 class HPNotebook(HP):
     def click(self):
         print('HP is clicking')
@@ -121,14 +141,13 @@ class DELLNotebook(DELL):
     def click(self):
         print('DELL is clicking')
 
+
 hp_notebook = HPNotebook()
 dell_notebook = DELLNotebook()
 hp_notebook.click()
 hp_notebook.scroll()
 dell_notebook.click()
 dell_notebook.scroll()
-
-
 
 print('################### exception ############################')
 
@@ -139,9 +158,11 @@ class InvaliPasswordException(Exception):
 
 def get_password():
     password = input('Please enter a pass: ')
-    if len(password) <8 :
+    if len(password) < 8:
         raise InvaliPasswordException()
     return password
+
+
 try:
     get_password()
 except InvaliPasswordException:
---
+++ employee.py
@@ -0,0 +1,82 @@
+import datetime
+
+
+class Employee:
+
+    num_of_emps = 0
+    raise_amt = 1.04
+
+    def __init__(self, first, last, pay=6000):
+        self.first = first
+        self.last = last
+        self.email = first + '.' + last + '@email.com'
+        self.pay = pay
+
+        Employee.num_of_emps += 1
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+    def apply_raise(self):
+        self.pay = int(self.pay * self.raise_amt)
+
+    def __repr__(self):
+        return f'Hi there {self.first}, {self.last} and my salary is: {self.pay}'
+
+    @classmethod
+    def set_raise_amt(cls, amount):
+        cls.raise_amt = amount
+
+    @classmethod
+    def from_string(cls, emp_str):
+        first, last, pay = emp_str.split('-')
+        return cls(first, last, pay)
+
+    @staticmethod
+    def is_workday(day):
+        if day.weekday() == 5 or day.weekday() == 6:
+            return False
+        return True
+
+
+class Manager(Employee):
+    def __init__(self, first, last, pay, employees=None):
+        super().__init__(first, last, pay)
+        if employees is None:
+            self.employees = []
+        else:
+            self.employees = employees
+
+    def print_employees(self):
+        for emp in self.employees:
+            print(emp)
+
+    def add_employee(self, employee):
+        if employee not in self.employees:
+            self.employees.append(employee)
+
+    def delete_employee(self, emp):
+        if emp in self.employees:
+            self.employees.remove(emp)
+
+
+emp_1 = Employee('Corey', 'Schafer', 5000)
+emp_2 = Employee('Mark', 'Mayer')
+emp_2.apply_raise()
+
+Employee.set_raise_amt(1.05)
+
+emp_str_1 = 'John-Doe-8000'
+new_emp_1 = Employee.from_string(emp_str_1)
+
+print(new_emp_1)
+
+my_date = datetime.date(2016, 7, 11)
+
+print(Employee.is_workday(my_date))
+
+manager = Manager('Adam', 'Smith', 1234, [emp_1, emp_2])
+manager.add_employee(new_emp_1)
+manager.print_employees()
+
+print(f'Num of emploees is {Employee.num_of_emps}')
