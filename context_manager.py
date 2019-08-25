ecc29c02d97bb5ab94c74084f9719c174af1477d
gmdmgithub <gmika@interia.pl>
2019-08-25 13:02:42 +0200

    generator final ver

--- context_manager.py
+++ context_manager.py
@@ -27,14 +27,40 @@ def open_file_with_function_context_manager():
     
     @contextmanager
     def open_file(file,mode):
-        f = open(file,mode)
-        yield f
-        f.close()
+        try:
+            f = open(file,mode)
+            yield f
+        except e:
+            print(e)
+        finally:
+            f.close()
 
     with open_file("test.txt","a") as f:
         f.write("\nhi there now we append with function context manager!")
 
     print(f.closed) #is file closed?
+
+
+import os
+
+def print_dirlist_with_function_context_manager():
+    
+    @contextmanager
+    def change_dir(destination):
+        try:
+            cwd = os.getcwd()
+            os.chdir(destination)
+            yield
+        except Exception as e: #any
+            print(e)
+        finally:
+            os.chdir(cwd)#back to original dir
+    
+    with change_dir("test"):
+        print(os.listdir())
+    with change_dir("thread"):
+        print(os.listdir())
+
 
 #################
 
@@ -42,6 +68,7 @@ if __name__ == "__main__":
     # first on class case
     #manage_file_class()
     #now with function
-    open_file_with_function_context_manager()
+    #open_file_with_function_context_manager()
+    print_dirlist_with_function_context_manager()
 
 
