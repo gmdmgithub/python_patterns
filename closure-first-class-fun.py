265ab56594c030d15f495c1489fc40e7c73e8fba
gmdmgithub <gmika@interia.pl>
2019-09-02 19:43:13 +0200

    amends

--- closure-first-class-fun.py
+++ closure-first-class-fun.py
@@ -3,7 +3,7 @@ import logging
 logging.basicConfig(filename='example.log', level=logging.INFO)
 
 
-print('################# closuer main example ##########')
+print('################# closure main example ##########')
 def logger(func):
     def log_func(*args):
         logging.info(
@@ -28,30 +28,30 @@ sub_logger(10, 5)
 sub_logger(20, 10)
 
 
-print('################# closuer simple explanation- ##########')
-#simpler explenation
+print('################# closure simple explanation- ##########')
+#simpler explanation
 def outer_func():
     message = 'Hi from outer'
 
-    def iner_fun():
+    def inner_fun():
         print(message) #free variable
 
-    return iner_fun()
+    return inner_fun()
 
-outer_func() # simpler because function is returnig with result "()" means we execute the function
+outer_func() # simpler because function is returning with result "()" means we execute the function
 
-print('################# now just returnig function - not value ##########')
-#now just returnig function - not value
+print('################# now just returning function - not value ##########')
+#now just returning function - not value
 def outer_func2(add_message):
     message = f'Hi from outer func 2 with passed message: {add_message}'
 
-    def iner_fun2():
+    def inner_fun2():
         print(message) #free variable
 
-    return iner_fun2
+    return inner_fun2
 
 my_func = outer_func2('something strange')
-# and surprise - clouser is inner function that remember and has access to the values even after executing
+# and surprise - clouter is inner function that remember and has access to the values even after executing
 print(my_func.__name__)
 my_func()
 
@@ -64,5 +64,5 @@ def html_tag(tag):
 
 print_h1 = html_tag('h1')
 print_h1('Test the function with h1 inside')
-print_paragraphe = html_tag('P')
-print_paragraphe('New paragraphe from closure')
+print_paragraph = html_tag('P')
+print_paragraph('New paragraph from closure')
--- lambda.py
+++ lambda.py
@@ -1,3 +1,4 @@
+#first example - lambda fun is used to title some sting and first strip (trim)
 full_name = lambda fn,ln : fn.strip().title() + " "+ln.strip().title()
 
 print(full_name(' greg    ',' doe'))
@@ -13,9 +14,9 @@ for name in names:
 print(sorted(names, key=lambda name: name.split(" ")[-1].lower()))
 
 student_tuples = [
-    ('zohn', 'A', 15),
+    ('john', 'A', 15),
     ('Cane', 'C', 12),
-    ('dave', 'B', 10)
+    ('Dave', 'B', 10)
 ]
 sorted_stud = sorted(student_tuples, key=lambda student: student[2]) # by age
 print(sorted_stud)
@@ -38,16 +39,17 @@ print(quadratic_func(1,3,5)(3))
 print(20*'#','filter and lambda',20*'#')
 num_list = [2,3,4,5,67,43,23,12,34,56]
 print('original number list',num_list)
-even_num = filter(lambda x: x%2==0,num_list)
+even_num = filter(lambda x: x%2 == 0, num_list)
 print('even from list',list(even_num))
 
 ################## map and lambda ##########################
 print(20*'#','map and lambda',20*'#')
-triple_list = map(lambda n:n*3,num_list)
+triple_list = map(lambda n:n*3, num_list)
 print('tripled list',list(triple_list))
 
 ################## reduce and lambda ##########################
-from functools import reduce
+from functools import reduce #reduce is not build-in needs to be import
+
 print(20 * '#', 'reduce and lambda', 20 * '#')
-reduced_list_sum = reduce(lambda x,y:x+y,num_list, -20)#initial valu is -20
-print('sum with reduce - initial value -20',reduced_list_sum)
+reduced_list_sum = reduce(lambda x,y:x+y, num_list, -20)#initial value is -20
+print('sum with reduce - initial value -20', reduced_list_sum)
